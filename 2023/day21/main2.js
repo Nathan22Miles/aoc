ff0403a0dfd5879869a52bf93adf1a204eb1c92a
Nathan Miles <milesnl@gmail.com>
2025-01-18 06:14:10 -0600

    cleanup

--- 2023/day21/main2.js
+++ 2023/day21/main2.js
@@ -20,6 +20,7 @@ let _m = new Maze(data, { addBorder: true, borderChar: '#' })
 let { m, r, c } = _m
 log(r, c, m[0].length)
 
+// points at which path can enter a maze instance
 let entriesRC = [
     [1, 1], [66, 1], [131, 1],
     [1, 66], [66, 66], [131, 66],
@@ -28,8 +29,17 @@ let entriesRC = [
 
 let entries = entriesRC.map(rc => _m.rc2p(rc)) // p's
 
+let oddCount = doSteps(entries[4], 265)
+let evenCount = doSteps(entries[4], 266)
 
-function go(pStart, stepCount) {
+let doubleStride = 131+131
+
+let stride = 131 // number steps to cross a maze instance
+let halfStride = 66 // number of steps from middle to edge+1
+
+let totalSteps = 26501365
+
+function doSteps(pStart, stepCount) {
     let vs = new Set([pStart])
 
     rng(stepCount, _ => {
@@ -37,37 +47,24 @@ function go(pStart, stepCount) {
         vs.forEach(v => _vs._add(_m.neighbors(v, 1)))
         vs = _vs
     })
-    
+
     return vs.size
 }
 
-let oddCount = go(entries[4], 265)
-let evenCount = go(entries[4], 266)
-let doubleStride = 131+131
-let stride = 131
-let halfStride = 66
-let totalSteps = 26501365
-
 function _calcCount(entry, stepCount) {
-    let result = go(entries[entry], stepCount)
+    let result = doSteps(entries[entry], stepCount)
     return result
 }
 const calcCount = memoize(_calcCount)
 
-function goCount(entry, stepCount) {
-    stepCount = stepCount % doubleStride
-    return calcCount(entry, stepCount)
-}
-
- function verticalCount(entry, stepCount) {
+function verticalCount(entry, stepCount) {
     let _count = 0
     let doubleStrides = Math.floor(stepCount / doubleStride)
-    
     _count += doubleStrides * (oddCount + evenCount)
-    stepCount = stepCount - doubleStride*doubleStrides
-
+    
+    stepCount = stepCount % doubleStride
     for (; stepCount > 0; stepCount -= stride) {
-        _count += goCount(entry, stepCount)
+        _count += calcCount(entry, stepCount % doubleStride)
     }
 
     return _count
@@ -82,14 +79,13 @@ count += verticalCount(7, totalSteps - halfStride) // upper middle
 let stepCount = totalSteps - halfStride
 
 for (; stepCount > 0; stepCount -= stride) {
-    count += goCount(3, stepCount) // right from middle
-    count += goCount(5, stepCount) // left from middle
+    count += calcCount(3, stepCount % doubleStride) // right from middle
+    count += calcCount(5, stepCount % doubleStride) // left from middle
 
     count += verticalCount(0, stepCount - halfStride) // lower right
     count += verticalCount(2, stepCount - halfStride) // lower left
     count += verticalCount(6, stepCount - halfStride) // upper right
     count += verticalCount(8, stepCount - halfStride) // upper left
-    // break
 }
 
 log(count)
